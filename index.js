117f878fcb44e748e2043907879b86a542398c1d
yonatan arm <94531453+Yonatan-Arm@users.noreply.github.com>
2022-01-26 20:14:44 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,100 +1,118 @@
-'use strict'
+"use strict";
 
 var lives = 3;
-const Boomb='🔥'
+const Boomb = "🔥";
 var gboard;
-var count=0;
-const FLAG='🚩'
+var count = 0;
+const FLAG = "🚩";
+var countFlag = 0;
 var gLevel = {
-  SIZE: 4 ,
-  MINES:2 ,
-}
+  SIZE: 4,
+  MINES: 2,
+};
 
-function level(level){
-  if(level.innerText === 'easy'){
-    gLevel.SIZE=4
-    gLevel.MINES=2
-  } else if(level.innerText === 'hard'){
-    gLevel.SIZE=8
-    gLevel.MINES=12
-  }else{
-    gLevel.SIZE=12
-    gLevel.MINES=30
+function level(level) {
+  if (level.innerText === "easy") {
+    gLevel.SIZE = 4;
+    gLevel.MINES = 2;
+  } else if (level.innerText === "hard") {
+    gLevel.SIZE = 8;
+    gLevel.MINES = 12;
+  } else {
+    gLevel.SIZE = 12;
+    gLevel.MINES = 30;
   }
-  initGame()
+  restart();
 }
- 
 
 function initGame() {
-   gboard= buildBoard(gLevel.SIZE);
-   createBoomb(gboard ,gLevel.MINES);
-   printMat(gboard, ".board-container");
+  lives = 3;
+  count = 0;
+  gboard = buildBoard(gLevel.SIZE);
+  createBoomb(gboard, gLevel.MINES);
+  printMat(gboard, ".board-container");
 }
 
-
-function createBoomb(gboard , quntity){
-   var cells = checkCells(gboard)
-   while(quntity>0){
-   var rendomIdx = getRandomIntInt(0, cells.length)
-var boombIdx = cells[rendomIdx]
-gboard[boombIdx.i][boombIdx.j].isMine = true;
-   console.log(boombIdx);
-   quntity--
-     }
-}
-
-
-
-function cellClicked(elCell, cellI, cellJ) {
-  gboard[cellI][cellJ].isShown= true;
-  // console.log('elCell', elCell)
-  if (gboard[cellI][cellJ].isMine === true) {
-      // Update the Model:
-    gboard[cellI][cellJ] = Boomb
-      // Update the Dom:
-      elCell.innerText = Boomb
-      lives--
-      var live= document.querySelector('h2')
-      live.innerText= 'lives:'+ lives 
-  }else{
-    elCell.innerText = gboard[cellI][cellJ].minesAroundCount
-     expandShown(gboard, cellI, cellJ)
-    var counter=document.querySelector('.counter')
-    count++
-    counter.innerText = 'count:' + count
+function createBoomb(gboard, quntity) {
+  var cells = checkCells(gboard);
+  while (quntity > 0) {
+    var rendomIdx = getRandomIntInt(0, cells.length);
+    var boombIdx = cells[rendomIdx];
+    gboard[boombIdx.i][boombIdx.j].isMine = true;
+    quntity--;
+    console.log(boombIdx.i, boombIdx.j);
   }
 }
 
-function expandShown(mat,rowIdx, colIdx){
-    for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
-      if (i < 0 || i > mat.length - 1) continue
-      for (var j = colIdx - 1; j <= colIdx + 1; j++) {
-      if (j < 0 || j > mat[0].length - 1) continue
-      if (i === rowIdx && j === colIdx) continue
-      var currCell = mat[i][j]
-      if (currCell.isMine === false){
-        currCell.isShown= true;
-        renderCell({i: i, j: j}, currCell.minesAroundCount)
-        currCell.innerText= currCell.minesAroundCount
-        console.log(currCell);
-      } 
+function cellClicked(elCell, cellI, cellJ) {
+  gboard[cellI][cellJ].isShown = true;
+  // console.log('elCell', elCell)
+  if (gboard[cellI][cellJ].isMine === true) {
+    // Update the Model:
+    gboard[cellI][cellJ] = Boomb;
+    // Update the Dom:
+    elCell.innerText = Boomb;
+    lives--;
+
+    checkGameOver();
+    var live = document.querySelector("h2");
+    live.innerText = "lives:" + lives;
+  } else {
+    elCell.innerText = gboard[cellI][cellJ].minesAroundCount;
+    expandShown(gboard, cellI, cellJ);
+    var counter = document.querySelector(".counter");
+    count++;
+    counter.innerText = "count:" + count;
+  }
+}
+
+function expandShown(mat, rowIdx, colIdx) {
+  for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+    if (i < 0 || i > mat.length - 1) continue;
+    for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+      if (j < 0 || j > mat[0].length - 1) continue;
+      if (i === rowIdx && j === colIdx) continue;
+      var currCell = mat[i][j];
+      if (currCell.isMine === false) {
+        currCell.isShown = true;
+        renderCell({ i: i, j: j }, currCell.minesAroundCount);
+        currCell.innerText = currCell.minesAroundCount;
       }
     }
-    return count
-    }
-  
-     function checkGameOver(){
-       var popModal=document.querySelector('modal')
-       if(lives===0){
-         popModal.style.display="block"
-         popModal.innerText ='try better next time loser'
-       }
-     }
+  }
+  return count;
+}
 
-  
-     function rightButton(cell) {
-       cell.innerText = FLAG
-     }
- 
+function checkGameOver() {
+  var popModal = document.querySelector(".modal");
+  if (lives === 0) {
+    popModal.style.display = "inline";
+    popModal.innerText = "try better next time loser";
+  } else if (countFlag === gLevel.MINES) {
+    popModal.style.display = "inline";
+    popModal.innerText = "you are the winner";
+  }
+}
 
+function rightButton(cell, i, j) {
+  if (gboard[i][j].isMine === true) {
+    var counterFlag = document.querySelector(".counter-flag");
+    countFlag++;
+    counterFlag.innerText = "counter flags: " + countFlag;
+    checkGameOver();
+  }
+  cell.innerText = FLAG;
+}
 
+function restart() {
+  lives = 3;
+  count = 0;
+  countFlag=0;
+  var counter = document.querySelector(".counter");
+  counter.innerText = "count:" + count;
+  var counterFlag = document.querySelector(".counter-flag");
+  counterFlag.innerText = "counter flags: " + countFlag;
+  var popModal = document.querySelector(".modal");
+  popModal.style.display = "none";
+  initGame();
+}
