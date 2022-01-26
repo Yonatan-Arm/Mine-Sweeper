d321aabec40651ad705b5f83fdf2b037eeeb1ba8
yonatan arm <94531453+Yonatan-Arm@users.noreply.github.com>
2022-01-26 18:36:25 +0200

    Add files via upload

---
+++ help.js
@@ -0,0 +1,81 @@
+'use strict'
+
+ function buildBoard(SIZE){
+    var board = [];
+    for (var i = 0; i < SIZE; i++) {
+        board.push([]);
+        for (var j = 0; j < SIZE; j++) {
+            board[i][j] = createCell(board) ;
+        }
+    }
+    return board;
+}
+
+function createCell(){
+	var gcell={
+	minesAroundCount: 0,
+ isShown: false,
+ isMine: false,
+ isMarked: false
+	}
+return gcell;
+}
+
+
+  function printMat(mat, selector) {
+	var strHTML = '<table border="4px"><tbody>';
+	for (var i = 0; i < mat.length; i++) {
+	  strHTML += '<tr>';
+	  for (var j = 0; j < mat[0].length; j++) {
+		  var cell=mat[i][j];
+		 cell.minesAroundCount= countBoombsAround(mat,i ,j)
+		var className = 'cell cell-' + i + '-' + j;
+		strHTML += `<td class='${className}' + oncontextmenu="rightButton(this)" + onclick='cellClicked( this, ${i}, ${j})'></td> `
+	  }
+	  strHTML += '</tr>'
+	}
+	strHTML += '</tbody></table>';
+	var elContainer = document.querySelector(selector);
+	elContainer.innerHTML = strHTML;
+  }
+//   `<td data-${num} onclick="cellClicked(${num} , ${sizeArr})">${num}</td>`
+
+
+ function countBoombsAround(mat, rowIdx, colIdx) {
+	var count = 0
+	for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+	  if (i < 0 || i > mat.length - 1) continue
+	  for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+		if (j < 0 || j > mat[0].length - 1) continue
+		if (i === rowIdx && j === colIdx) continue
+		var currCell = mat[i][j]
+		if (currCell.isMine === true) count++
+	  }
+	}
+	return count
+  }
+
+  
+  function renderCell(location, value) {
+    // Select the elCell and set the value
+    var elCell = document.querySelector(`.cell-${location.i}-${location.j}`);
+	console.log(elCell);
+    elCell.innerHTML = value;
+  }
+
+   
+function checkCells(gBoard){
+	var arr=[]
+	for(var i=0; i<gBoard.length; i++){
+		for(var j=0; j<gBoard[i].length;j++){
+				arr.push({i:i, j:j})
+			
+		}
+	}
+	return arr
+}
+ 
+
+function getRandomIntInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
---
+++ index.js
@@ -0,0 +1,100 @@
+'use strict'
+
+var lives = 3;
+const Boomb='🔥'
+var gboard;
+var count=0;
+const FLAG='🚩'
+var gLevel = {
+  SIZE: 4 ,
+  MINES:2 ,
+}
+
+function level(level){
+  if(level.innerText === 'easy'){
+    gLevel.SIZE=4
+  } else if(level.innerText === 'hard'){
+    gLevel.SIZE=8
+    gLevel.MINES=12
+  }else{
+    gLevel.SIZE=12
+    gLevel.MINES=30
+
+  }
+  initGame()
+}
+ 
+
+function initGame() {
+   gboard= buildBoard(gLevel.SIZE);
+   createBoomb(gboard ,gLevel.MINES);
+   printMat(gboard, ".board-container");
+}
+
+
+function createBoomb(gboard , quntity){
+   var cells = checkCells(gboard)
+   while(quntity>0){
+   var rendomIdx = getRandomIntInt(0, cells.length)
+var boombIdx = cells[rendomIdx]
+gboard[boombIdx.i][boombIdx.j].isMine = true;
+   console.log(boombIdx);
+   quntity--
+     }
+}
+
+
+
+function cellClicked(elCell, cellI, cellJ) {
+  gboard[cellI][cellJ].isShown= true;
+  // console.log('elCell', elCell)
+  if (gboard[cellI][cellJ].isMine === true) {
+      // Update the Model:
+    gboard[cellI][cellJ] = Boomb
+      // Update the Dom:
+      elCell.innerText = Boomb
+      lives--
+      var live= document.querySelector('h2')
+      live.innerText= 'lives:'+ lives 
+  }else{
+    elCell.innerText = gboard[cellI][cellJ].minesAroundCount
+     expandShown(gboard, cellI, cellJ)
+    var counter=document.querySelector('.counter')
+    count++
+    counter.innerText = 'count:' + count
+  }
+}
+
+function expandShown(mat,rowIdx, colIdx){
+    for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+      if (i < 0 || i > mat.length - 1) continue
+      for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+      if (j < 0 || j > mat[0].length - 1) continue
+      if (i === rowIdx && j === colIdx) continue
+      var currCell = mat[i][j]
+      if (currCell.isMine === false){
+        currCell.isShown= true;
+        renderCell({i: i, j: j}, currCell.minesAroundCount)
+        currCell.innerText= currCell.minesAroundCount
+        console.log(currCell);
+      } 
+      }
+    }
+    return count
+    }
+  
+     function checkGameOver(){
+       var popModal=document.querySelector('modal')
+       if(lives===0){
+         popModal.style.display="block"
+         popModal.innerText ='try better next time loser'
+       }
+     }
+
+  
+     function rightButton(cell) {
+       cell.innerText = FLAG
+     }
+ 
+
+
